5a5e91c2f47035dd4aa6991b7d3e831a38cc6439
arugo11 <nezuminoarugo1111@gmail.com>
2024-07-17 17:12:13 +0900

    [add] io_handlerの機能追加
    実数のみ1次元,複素数1次元,実数2次元の例に対応するように変更

---
+++ io_handler.c
@@ -0,0 +1,18 @@
+/***
+ * @file io_handler.c
+ * @brief ファイルの入出力関係の処理
+ * @author argo
+ * @date 2024/07/17
+*/
+
+#include <stdio.h>
+
+#include "io_handler.h"
+
+int read_signal_from_file(const char* filename, double* output, int max_size)
+{
+    
+}
+
+
+

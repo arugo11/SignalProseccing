2e94ab3989a14a643f79d0685e357d37ad4903a0
arugo11 <nezuminoarugo1111@gmail.com>
2024-07-17 17:20:11 +0900

    [add] io_handlerの機能追加
    実数のみ1次元,複素数1次元,実数2次元の例に対応するように変更

--- io_handler.h
+++ io_handler.h
@@ -1,22 +1,62 @@
 #ifndef IO_HANDLER_H
 #define IO_HANDLER_H
 
-/**
- * @brief テキストファイルから信号を読み込む
- * @param filename ファイル名
- * @param output 読み込んだ信号データの出力先
- * @param max_size 読み込む最大サイズ
- * @return 実際に読み込んだデータの数
- */
-int read_signal_from_file(const char* filename, double* output, int max_size);
+#include "complex.h"
 
 /**
- * @brief 信号をテキストファイルに書き込む
- * @param filename ファイル名
- * @param signal 書き込む信号データ
- * @param size 信号データのサイズ
- * @return 成功時は0、失敗時は-1
+ * @brief 1次元の実数データをファイルから読み込む
+ * @param filename 入力ファイル名
+ * @param output 読み込んだデータを格納する配列
+ * @param max_size 読み込む最大要素数
+ * @return 実際に読み込んだ要素数、エラーの場合は負の値
  */
-int write_signal_to_file(const char* filename, const double* signal, int size);
+int read_real_1d(const char* filename, double* output, int max_size);
+
+/**
+ * @brief 1次元の実数データをファイルに書き込む
+ * @param filename 出力ファイル名
+ * @param data 書き込むデータの配列
+ * @param size データの要素数
+ * @return 成功した場合は0、失敗した場合は負の値
+ */
+int write_real_1d(const char* filename, const double* data, int size);
+
+/**
+ * @brief 1次元の複素数データをファイルから読み込む
+ * @param filename 入力ファイル名
+ * @param output 読み込んだデータを格納する配列
+ * @param max_size 読み込む最大要素数
+ * @return 実際に読み込んだ要素数、エラーの場合は負の値
+ */
+int read_complex_1d(const char* filename, Complex* output, int max_size);
+
+/**
+ * @brief 1次元の複素数データをファイルに書き込む
+ * @param filename 出力ファイル名
+ * @param data 書き込むデータの配列
+ * @param size データの要素数
+ * @return 成功した場合は0、失敗した場合は負の値
+ */
+int write_complex_1d(const char* filename, const Complex* data, int size);
+
+/**
+ * @brief 2次元の実数データをファイルから読み込む
+ * @param filename 入力ファイル名
+ * @param output 読み込んだデータを格納する2次元配列
+ * @param max_rows 読み込む最大行数
+ * @param max_cols 読み込む最大列数
+ * @return 実際に読み込んだ行数、エラーの場合は負の値
+ */
+int read_real_2d(const char* filename, double** output, int max_rows, int max_cols);
+
+/**
+ * @brief 2次元の実数データをファイルに書き込む
+ * @param filename 出力ファイル名
+ * @param data 書き込むデータの2次元配列
+ * @param rows データの行数
+ * @param cols データの列数
+ * @return 成功した場合は0、失敗した場合は負の値
+ */
+int write_real_2d(const char* filename, const double** data, int rows, int cols);
 
 #endif // IO_HANDLER_H
